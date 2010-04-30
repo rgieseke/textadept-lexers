4b6797955ea4046cd58e353c01f719b09b707d2b
rgieseke <none@none>
2010-04-30 12:23:03 +0200

    Combined Latex and Context lexer.

---
+++ tex.lua
@@ -0,0 +1,57 @@
+-- Copyright 2006-2010 Mitchell Foral mitchell<att>caladbolg.net. See LICENSE.
+-- April 2010 Robert Gieseke, combined LaTeX and ConTeXt lexing
+-- TeX LPeg lexer
+
+-- TODO: Embed LuaTeX
+--   ConTeXt: \directlua{...}
+--     or \startlua ... \stoplua
+--   LaTeX: see pdf 'LuaTEXtra references'
+
+local l = lexer
+local token, style, color, word_match = l.token, l.style, l.color, l.word_match
+local P, R, S = l.lpeg.P, l.lpeg.R, l.lpeg.S
+
+module(...)
+
+local ws = token('whitespace', l.space^1)
+
+-- comments
+local line_comment = '%' * l.nonnewline^0
+local block_comment = '\\begin{comment}' * (l.any - '\\end{comment}')^0 *
+  '\\end{comment}'
+local comment = token('comment', line_comment + block_comment)
+
+-- environment
+-- LaTeX environments
+local env_latex = '\\' * (P('begin') + 'end') * '{' *  word_match({
+  'abstract', 'array', 'center', 'description', 'displaymath', 'document',
+  'enumerate', 'eqnarray', 'equation', 'figure', 'flushleft', 'flushright',
+  'itemize', 'list', 'math', 'minipage', 'picture', 'quotation', 'quote',
+  'tabbing', 'table', 'tabular', 'thebibliography', 'theorem', 'titlepage',
+  'trivlist', 'verbatim', 'verse'
+}) * '}'
+local env_latex_math = '\\' * S('[]()') + '$' -- covers '$$' as well
+-- ConTeXt environments
+local env_context = '\\' * (P('start') * l.word + 'stop' * l.word)
+local environment = token('environment',  env_latex + env_latex_math +
+                                          env_context)
+
+-- commands
+local escapes = S('$%_{}&#')
+local command = token('keyword', '\\' * (l.word + escapes))
+
+-- operators
+local operator = token('operator', S('$&%#{}[]'))
+
+_rules = {
+  { 'whitespace', ws },
+  { 'comment', comment },
+  { 'environment', environment },
+  { 'keyword', command },
+  { 'operator', operator },
+  { 'any_char', l.any_char },
+}
+
+_tokenstyles = {
+  { 'environment', l.style_tag },
+}
