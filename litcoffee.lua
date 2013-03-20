26f71f9ae2ba325f926ef42839c87a6d61f07ce7
Robert Gieseke <robert.gieseke@gmail.com>
2013-03-20 12:38:17 +0100

    Update with lexers currently worked on

---
+++ litcoffee.lua
@@ -0,0 +1,19 @@
+-- Literate CoffeeScript LPeg lexer by Robert Gieseke.
+
+local l = lexer
+local token = l.token
+local P = lpeg.P
+
+local M = {_NAME = 'litcoffee'}
+
+-- Embedded in Markdown.
+local markdown = l.load('markdown')
+M._lexer = markdown
+
+-- Embedded CoffeeScript.
+local coffeescript = l.load('coffeescript')
+local coffee_start_rule = token(l.style_embedded, (P(' ')^4 + P('\t')))
+local coffee_end_rule = token(l.style_embedded, l.newline)
+l.embed_lexer(markdown, coffeescript, coffee_start_rule, coffee_end_rule)
+
+return M
